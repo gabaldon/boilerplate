cd4487eb0f552ab980d26ffd17f3dc3f7f0062bf
María Gabaldón Rico <mariag@stampery.co>
2021-05-31 17:54:45 +0200

    feat: add chart example

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,7 +98,12 @@ export default {
   modules: ['@nuxtjs/style-resources', '@nuxt/content', 'nuxt-i18n'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '~/plugins/lightweightcharts.js',
+      ssr: false,
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
